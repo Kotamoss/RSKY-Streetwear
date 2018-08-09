f6d39dc5e3c320e38a727c10b7662e80f0deafa7
Kotamoss <storyofko@gmail.com>
2018-08-09 17:52:59 -0400

    Added black no background logo in all relevant sizes and added it to launch screen

---
+++ RSKY-Streetwear/CategoriesVC.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RSKY-Streetwear
+//
+//  Created by R$KY on 7/15/18.
+//  Copyright © 2018 Dakotah Moss. All rights reserved.
+//
+
+import UIKit
+
+// Changing ViewController to something more relevant
+class CategoriesVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
